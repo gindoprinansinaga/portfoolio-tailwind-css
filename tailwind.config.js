2a650683f2eea99199ced94e2566002b5dd181a1
gindoprinandosinaga <student.sinaga@gmail.com>
2022-04-18 12:07:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container:
+    {
+      center: true,
+      padding: '16px'
+    },
+    extend: 
+    {
+      colors:
+      {
+        primary: '#22c55e',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens :
+      {
+        '2xl' : '1320px',
+      },
+      fontFamily:
+      {
+        inter: ['Inter']
+      }
+    },
+  },
+  plugins: [],
+}
