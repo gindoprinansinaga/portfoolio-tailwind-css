c7631b7d1de296acea8f9210d7e374cd52f421e5
gindoprinandosinaga <student.sinaga@gmail.com>
2022-04-20 15:50:35 +0700

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
-    container:
-    {
+    container: {
       center: true,
-      padding: '16px'
+      padding: "16px",
     },
-    extend: 
-    {
-      colors:
-      {
-        primary: '#22c55e',
-        secondary: '#64748b',
-        dark: '#0f172a'
+    extend: {
+      colors: {
+        primary: "#22c55e",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
-      screens :
-      {
-        '2xl' : '1320px',
+      screens: {
+        "2xl": "1320px",
       },
-      fontFamily:
-      {
-        inter: ['Inter']
-      }
+      fontFamily: {
+        inter: ["Inter"],
+      },
     },
   },
   plugins: [],
-}
+};
